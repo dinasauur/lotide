82803ff14b4c825a73997d89a951f8a349d59c47
Wendina <wendina.teng@gmail.com>
2023-03-07 03:38:06 +0000

    Added a comment with a peer's solution.

--- tail.js
+++ tail.js
@@ -25,3 +25,11 @@ const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
+/*
+Solution from a peer. Need to go over this portion again to fully understand:
+
+for(let i = 1; i < words.length; i++){
+  assertEqual(words[i], tail(words)[i-1]);
+}
+
+*/
