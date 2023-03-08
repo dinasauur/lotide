e6fec1b7f5474c5f740fd58fe1671cf316f90061
Wendina <wendina.teng@gmail.com>
2023-03-08 06:31:41 +0000

    Function that compares two different arrays using for loop.

---
+++ eqArray.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (list1, list2){
+  for (let i = 0; i < list1.length; i++) {
+    if (list1[i] !== list2[i]){
+      return false;
+    } 
+  } return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [1, 2, 5])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
