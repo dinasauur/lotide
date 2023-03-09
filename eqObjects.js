612a366a4366c2f6d462b076fcee3aecfe384906
Wendina <wendina.teng@gmail.com>
2023-03-09 21:53:06 +0000

    Cleaned up code to make it look prettier.

--- eqObjects.js
+++ eqObjects.js
@@ -1,5 +1,7 @@
 const eqArrays = function(list1, list2) {
+
   for (let i = 0; i < list1.length; i++) {
+
     if (list1[i] !== list2[i]) {
       return false;
     }
@@ -7,33 +9,29 @@ const eqArrays = function(list1, list2) {
 };
 
 const assertEqual = function(actual, expected) {
+
   if (actual === expected) {
     return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   }
-  if (actual !== expected) {
-    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
+  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
 };
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 
 const eqObjects = function(object1, object2) {
+
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
 
-  // Condition 1 - If they have same number of keys.
-  //// Cannot use.length because it's an object. Can create an array with Object.keys()
-  // Condition 2 - If the value for each key in one object === value of the same key in other object
-  //// Loop through each keys and compare the values for condition 2
-  //// Return false if there is no match
-  ////End of Loop
-  // Return true
   if (keys1.length !== keys2.length) {
     return false;
   }
+
   for (let key of keys1) {
+
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
@@ -43,7 +41,8 @@ const eqObjects = function(object1, object2) {
     if (object1[key] !== object2[key]) {
       return false;
     }
-  } return true;
+  }
+  return true;
 };
 
 
