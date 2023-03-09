abb2d17ef72d51f716b7cb83d7d27a4a906519f1
Wendina <wendina.teng@gmail.com>
2023-03-09 08:28:05 +0000

    Add letterPositions function that returns all indices in the string where each character is found. Return multiple numbers, not just one.

---
+++ letterPositions.js
@@ -0,0 +1,54 @@
+// return all the indices in the string where each character is found
+// return an array of the indices for each letter
+// output is an object with arrays as values for the properties
+
+// Write a function
+
+// Write a buffer
+
+// Loop through the parameter(sentence)
+// Condition - if new object does not have that letter, add it to the object and count it
+// Fi
+
+
+// Return buffer
+
+//
+const eqArrays = function(list1, list2) {
+  for (let i = 0; i < list1.length; i++) {
+    if (list1[i] !== list2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return `✅✅✅ Assertion Passed`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed`;
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  const newSentence = sentence.toLowerCase().split(' ').join('');
+  for (let index = 0; index < newSentence.length; index++) {
+    // index -> indices
+    // newSentence[index] -> actual value
+    const value = newSentence[index];
+
+    if (results[value]) {
+      results[value].push(index);
+    } else {
+      results[value] = [index];
+    }
+  } return results;
+};
+
+
+
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
+
+
