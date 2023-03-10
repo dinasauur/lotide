5836a548fb7c63afd01772a8a1e22f821b15df63
Wendina <wendina.teng@gmail.com>
2023-03-10 00:04:38 +0000

    Adding a map function that is supposed to resemble the built-in .map() method.

---
+++ map.js
@@ -0,0 +1,33 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+};
+
+const eqArrays = function(list1, list2) {
+  for (let i = 0; i < list1.length; i++) {
+    if (list1[i] !== list2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+//METHOD 1 - calling the eqArrays function inside the asserArraysEqual function.
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const results1 = map(words, word => word[0]);
+
+console.log(assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']));
